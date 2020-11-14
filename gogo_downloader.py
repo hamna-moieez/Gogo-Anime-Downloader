046cec0d8be3c374b2bfd599ca77549b235368e5
hamna-moieez <hamna21797@gmail.com>
2020-11-14 13:15:21 +0500

    base code skeleton

---
+++ gogo_downloader.py
@@ -0,0 +1,10 @@
+"""
+This is the base downloader class.
+"""
+
+class GogoDownloader:
+    def __init__(self, baseURl):
+        """
+        Constructor
+        """
+        self.baseURL = baseURL
